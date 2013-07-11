a3817d989cacc2bea4810a6b0df8161a0dd1585c
John J Murdoch <murdochjohn.github@moumantai.de>
2013-07-12 00:50:25 +0200

    added some tests

--- test/unit/controller/ListViewControllerSpec.js
+++ test/unit/controller/ListViewControllerSpec.js
@@ -11,13 +11,24 @@ describe('In ListViewController', function() {
     rootScope = $rootScope;
     scope = $rootScope.$new();
 
-    controller = $controller("BoardViewController", {
-      $scope: scope,
+    controller = $controller("ListViewController", {
+      $scope: scope
     })
   }));
 
   describe('after initialization', function() {
 
+    it("should set the property hidden to 'true'", function() {
+      expect(scope.hidden).toBe(true);
+    });
+
+    it("should a function be bound to 'togglePrintAllNone'", function() {
+      expect(scope.togglePrintAllNone).toEqual(jasmine.any(Function));
+    });
+
+    it("should a function be bound to 'toggleShowHide'", function() {
+      expect(scope.toggleShowHide).toEqual(jasmine.any(Function));
+    });
   });
   
 });
