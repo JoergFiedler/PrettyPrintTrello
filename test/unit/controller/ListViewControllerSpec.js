9bc585db49ec7b99d2f62e12d3b59ea00a0d472f
John J Murdoch <murdochjohn.github@moumantai.de>
2013-07-12 22:23:05 +0200

    added missing tests

--- test/unit/controller/ListViewControllerSpec.js
+++ test/unit/controller/ListViewControllerSpec.js
@@ -1,4 +1,4 @@
-describe('In ListViewController', function() {
+describe('List View Controller', function() {
 
   var rootScope,
     scope;
@@ -16,7 +16,7 @@ describe('In ListViewController', function() {
     })
   }));
 
-  describe('after initialization', function() {
+  describe('controller initialization', function() {
 
     it("should set the property hidden to 'true'", function() {
       expect(scope.hidden).toBe(true);
@@ -30,5 +30,46 @@ describe('In ListViewController', function() {
       expect(scope.toggleShowHide).toEqual(jasmine.any(Function));
     });
   });
-  
+
+  describe('function toggleShowHide', function() {
+
+    it("should toggle scope property 'hidden'", function() {
+      scope.toggleShowHide();
+      expect(scope.hidden).toBeFalsy();
+
+      scope.toggleShowHide();
+      expect(scope.hidden).toBeTruthy();
+    });
+  });
+
+  describe('function togglePrintAllNone', function(){
+
+    beforeEach(function(){
+      spyOn(scope, '$broadcast');
+    });
+
+    it("should toggle scope property 'printAll'", function() {
+      scope.togglePrintAllNone();
+      expect(scope.printAll).toBeTruthy();
+
+      scope.togglePrintAllNone();
+      expect(scope.printAll).toBeFalsy();
+    });
+
+    it("should broadcast event 'tpp:list:print:add' when scope property 'printAll' is false", function() {
+      scope.printAll = false;
+      scope.togglePrintAllNone();
+
+      expect(scope.$broadcast).toHaveBeenCalledWith('tpp:list:print:add');
+    });
+
+    it("should broadcast event 'tpp:list:print:remove' when scope property 'printAll' is true", function() {
+      scope.printAll = true;
+      scope.togglePrintAllNone();
+
+      expect(scope.$broadcast).toHaveBeenCalledWith('tpp:list:print:remove');
+    });
+  });
+
+
 });
