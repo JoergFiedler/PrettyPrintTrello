5a9c93c35c220558a686e05f98e32841f8491324
Alexander Weiher <alexander.weiher_external@immobilienscout24.de>
2014-05-14 17:26:11 +0200

    implement feature-request: Allow color selection for cards

--- app/script/controller/PreviewController.js
+++ app/script/controller/PreviewController.js
@@ -1,6 +1,15 @@
 angular.module('PrettyPrintTrello').controller('PreviewController', function PreviewController($scope, googleExtensionApiService) {
   "use strict";
 
+  var labelsToColorMap = {
+    "green": "#34b27d",
+    "yellow": "#dbdb57",
+    "orange": "#e09952",
+    "red": "#cb4d4d",
+    "purple": "#93c",
+    "blue": "#4d77cb"
+  };
+
   function init() {
     $scope.$emit('tpp:action', { label: "Print'em All", 'event': 'tpp:print'});
   }
@@ -9,7 +18,22 @@ angular.module('PrettyPrintTrello').controller('PreviewController', function Pre
     googleExtensionApiService.print(angular.element('#tpp-print-frame').html());
   }
 
+  function getCardColorByLabel(item) {
+    var firstLabelColor = item.labels.length ? item.labels[0].color : 'yellow';
+    return labelsToColorMap[firstLabelColor];
+  }
+
+  function getStoryStyle(item) {
+    var color = $scope.cardColor ? $scope.cardColor : getCardColorByLabel(item);
+
+    return {
+      border: '10px solid ' + color
+    }
+  }
+
   $scope.init = init;
-  $scope.$on('tpp:print', print)
+  $scope.getStoryStyle = getStoryStyle;
+
+  $scope.$on('tpp:print', print);
 });
 
